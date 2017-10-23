96b72ed58ab2d5512923569beb27d179a24b2689
david morse <davidbryanmarshall@gmail.com>
2017-10-22 19:42:33 -0700

    add npm package: colors

---
+++ bamazonCustomer.js
@@ -0,0 +1,18 @@
+var mysql = require('mysql');
+var inquirer = require('inquirer');
+var table = require('cli-table');
+var colors = require('colors');
+
+var connection = mysql.createConnection({
+  host: 'localhost',
+  port: 3306,
+  user: 'root',
+  password: 'root',
+  database: 'bamazonDB'
+});
+
+connection.connect(function(err) {
+  if (err) throw err;
+  console.log('connected, whoop!')
+  // runSearch();
+});
