054b8f51476c6f80b54cc2ee2c9f3c905f52661b
david morse <davidbryanmarshall@gmail.com>
2017-10-22 20:27:31 -0700

    completed scheme with products for Challenge #1: Customer View

--- schema.sql
+++ schema.sql
@@ -4,7 +4,7 @@ CREATE database bamazonDB;
 USE bamazonDB;
 
 CREATE TABLE products (
-  item_id INT NOT NULL,
+  item_id INT AUTO_INCREMENT NOT NULL,
   product_name VARCHAR(100) NULL,
   product_sales DECIMAL(10,2) NULL,
   department_name VARCHAR(100) NULL,
@@ -12,6 +12,20 @@ CREATE TABLE products (
   stock_quantity INT NOT NULL,
   PRIMARY KEY (item_id)
 );
+
+INSERT INTO products
+    (product_name, product_sales, department_name, price, stock_quantity)
+VALUES
+    ("Monopoly", 0, "Games", 14.89, 7),
+    ("Scrabble", 0, "Games", 9.99, 6),
+    ("Futon", 0, "Furniture", 277.00, 2),
+    ("Coffee Table", 0, "Children", 144.99, 10),
+    ("Toothpaste", 0, "Personal Hygeine", 4.19, 48),
+    ("Comb", 0, "Personal Hygeine", 1.49, 78),
+    ("Flat Ccreen TV", 0, "Electronics", 349.99, 7),
+    ("Bluetooth Speaker", 0, "Electronics", 44.96=5, 35),
+    ("Handheld Whiteboard", 0, "Office Supplies", 7.39, 62),
+    ("Wire Mesh Desk Organizer", 0, "Office Supplies", 18.50, 4);
 
 SELECT * FROM products;
 
